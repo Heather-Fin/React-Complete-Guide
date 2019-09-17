64fe40170c2c986eb18c3a1f929b25bc82da0abd
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-09-17 16:29:13 -0500

    Added state to person component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Heather', age: 24},
+      {name: 'Maya', age: 3.5},
+      {name: 'Kimora', age: 8}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Heather" age="24" />
-        <Person name="Maya" age="3.5">My Hobbies: Sleeping</Person>
-        <Person name="Kimora" age="8" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Sleeping</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
