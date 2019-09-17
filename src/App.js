5616ba60c8cfbb32e4ef311bb6f1041e4e1f3a04
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-09-17 16:14:21 -0500

    Add person props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
+        <Person name="Heather" age="24" />
+        <Person name="Maya" age="3.5">My Hobbies: Sleeping</Person>
+        <Person name="Kimora" age="8" />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
