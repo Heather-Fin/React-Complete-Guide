d25eb586fff5e976fdca16c40f6bd4fef15ed61b
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-09-17 16:55:15 -0500

    Add hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Heather', age: 24},
       {name: 'Maya', age: 3.5},
       {name: 'Kimora', age: 8}
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Krista';
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: 'Krista', age: 54},
         {name: 'Maya', age: 3.5},
@@ -22,19 +20,16 @@ class App extends Component {
       ]
     });
   }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Sleeping</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
-  }
+  
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Sleeping</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
-export default App;
+export default app;
